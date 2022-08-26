862c81f9251dbb78d41f61afdf1244bf1ac83e33
Benjamin Mishra <benjamin.mishra@ringtailgames.com>
2022-08-26 10:33:36 +0530

    Initial commit

---
+++ src/classes/StaticBlock.ts
@@ -0,0 +1,24 @@
+class Foo {
+    private static count = 0;
+ 
+    static {
+        console.log("Hello World");
+    }
+}
+
+class AClass {
+    private static count = 0;
+ 
+    public static constructor(){
+
+    }
+}
+
+function loadLastInstances() :string {
+    return "Instance";
+}
+
+
+let foo = new Foo();
+
+
